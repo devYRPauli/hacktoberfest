239a42a94ff86cf612d66ffd3282c51a2da5d87f
Yash Raj Pandey <55940078+devYRPauli@users.noreply.github.com>
2019-10-01 18:17:32 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,10 @@
+def triangle():
+    N=0
+    trianglerow=int(input())
+    for i in range(0, trianglerow):
+         print("\n")
+         N=N+1
+         for i in range(0, N):
+            print("i", end="")
+
+triangle()
