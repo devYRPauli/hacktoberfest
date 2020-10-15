bc493d0ea9f71454b992e4d8d450c4093cf6865b
piyush-madda <72153460+piyush-madda@users.noreply.github.com>
2020-10-15 11:25:25 +0530

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,10 +1,7 @@
+  
 def triangle():
-    N=0
     trianglerow=int(input())
     for i in range(0, trianglerow):
-         print("\n")
-         N=N+1
-         for i in range(0, N):
-            print("i", end="")
-
+        print('i'*(i+1))    #no need to use nested loop
+        print()
 triangle()
